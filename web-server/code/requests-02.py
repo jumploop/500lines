d1730f8e9380db9a9990df436a75f99360e3d94e
liming <827182486@qq.com>
2022-04-23 21:50:14 +0800

    requests访问谷歌需要代理

--- web-server/code/requests-02.py
+++ web-server/code/requests-02.py
@@ -1,5 +1,10 @@
 from __future__ import print_function
 import requests
+proxies = {
+  'http': 'http://127.0.0.1:10800',
+  'https': 'http://127.0.0.1:10800',
+}
 parameters = {'q' : 'Python', 'client' : 'Firefox'}
-response = requests.get('http://www.google.com/search', params=parameters)
+# 国内访问需要代理
+response = requests.get('http://www.google.com/search', params=parameters ,proxies=proxies)
 print('actual URL:', response.url)
